34bd12d59e1ded95a56a6c0fb49313c5760b197f
amirbourvine <amirbourvine@gmail.com>
2024-03-04 02:52:11 +0200

    rpm_for_fid written

--- src/DjangoCommunication.jsx
+++ src/DjangoCommunication.jsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
 import axios from 'axios';
+import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
 
 export function getQueryTypes() {
   // const [types, setTypes] = React.useState([]);
@@ -31,7 +39,7 @@ export function getQueriesOfType(type) {
   // console.log(queries);
   switch(type){
     case 'Heat Map':
-      return ['HeatMap_1', 'HeatMap_2']
+      return ['HeatMap_1', 'HeatMap_2', 'RPM_FOR_FID']
     
     case 'Marker Map':
       return ['MarkerMap_1', 'MarkerMap_2']
@@ -44,7 +52,13 @@ export function getInputParams(query){
     if(query == '')
         return [];
 
-    return [query + "_param1", query + "_param2", query + "_param3"]
+    switch(query){
+      case 'RPM_FOR_FID':
+        return ['fid']
+    }
+    
+
+    return ([query + "_param1", query + "_param2", query + "_param3"].concat(["region", "fromDate", "toDate", "fromTime", "toTime"]));
 }
 
 function queryNameToQueryTemplate(query){
@@ -52,11 +66,22 @@ function queryNameToQueryTemplate(query){
   //for param in 
   //{region, dateFrom, dateTo, timeFrom, timeTo} U {sepcificParam | specificParam is in getInputParams(query)}
   //$param$ will be replaced with the given input param
-  const param1 = 'param1: %' + query + "_param1%,"  
-  const param2 = 'param2: %' + query + "_param2%,"
-  const param3 = 'param3: %' + query + "_param3%,"
+  
+  switch(query){
+    case 'RPM_FOR_FID':
+      let built_query = `query try1 {
+        heat_map(query: "select tele_pp_lat as lat,tele_pp_long as lon,tele_rpm as strength from fast_params where fid=%fid% and tele_pp_lat!=0 and tele_pp_long!=0") {lat lon strength}
+      }`
+      return built_query
+  }
 
-  return param1 + " " + param2 + " " + param3 + " region: %region%, from date: %fromDate%, to date: %toDate%, from time: %fromTime%, to time: %toTime%"
+  let param_list = getInputParams(query)
+  let str = ""
+  for(i=0; i<param_list.length; i++){
+    str += param_list[i]+": %"+param_list[i]+"%,"
+  }
+
+  return str + " region: %region%, from date: %fromDate%, to date: %toDate%, from time: %fromTime%, to time: %toTime%"
 }
 
 function replaceEmptyParamWithValue(paramNames, paramVals, template){
@@ -73,18 +98,48 @@ export function getFinalQuery(jsonParams){
 
   const template = queryNameToQueryTemplate(jsonParams.query);
 
-  const paramNames = getInputParams(jsonParams.query).concat(["region", "fromDate", "toDate", "fromTime", "toTime"]);
+  const paramNames = getInputParams(jsonParams.query)
   
-  let inputParamsVals = getInputParams(jsonParams.query).map((param) => (
+  let paramVals = getInputParams(jsonParams.query).map((param) => (
     jsonParams[param]
   ));
 
-  const paramVals = inputParamsVals.concat([jsonParams.region, jsonParams.dateFrom, 
-    jsonParams.dateTo, jsonParams.timeFrom, jsonParams.timeTo]);
-
   console.log(paramVals);
 
-  return replaceEmptyParamWithValue(paramNames, paramVals, template);//queryNameToQueryTemplate(query);
+  return replaceEmptyParamWithValue(paramNames, paramVals, template);
+}
+
+function fix_data_structure(data, query_name){
+  switch(query_name){
+    case 'RPM_FOR_FID':
+      let res =  ((data["data"])["heat_map"]).map((dict) => (
+        (dict["lat"],dict["lon"],dict["strength"])
+      ));
+      return res;
+  }
+
+  return null
+}
+
+
+export async function exeQuery(jsonParams){
+  let query = getFinalQuery(jsonParams)
+
+  console.log(query)
+
+  const {data, error} = await client.query({
+    query: query
+  })
+  if (error) {
+      console.error('Error executing query: ', error);
+      return;
+  }
+
+  console.log(data)
+
+  let res = fix_data_structure(data)
+  
+  console.log(res)
 }
 
 
