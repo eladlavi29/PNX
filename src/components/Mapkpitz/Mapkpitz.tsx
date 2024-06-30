4ede78b7da6b6ec88b4af34407e3098405aeac05
omerdaube <102986147+omerdaube@users.noreply.github.com>
2024-06-30 11:22:10 +0300

    update params & topbar

--- src/components/Mapkpitz/Mapkpitz.tsx
+++ src/components/Mapkpitz/Mapkpitz.tsx
@@ -4,7 +4,7 @@ import {
   Popup,
   TileLayer,
   useMapEvents,
-  Polyline
+  Polyline,
 } from "react-leaflet";
 import { useState, useEffect, useRef } from "react";
 import RotatedMarker from "./RotatedMarker";
@@ -23,12 +23,29 @@ import "leaflet-draw/dist/leaflet.draw.css";
 import antenas_json from "../../antenas_config.json";
 
 type direction = 1 | -1;
-const path = [
-  [31.5, 34.75],
-  [31.55, 34.85],
-  [31.65, 34.65],
-  [31.85, 34.95],
+const colors = [
+  "#2C3E50", // Midnight Blue
+  "#8E44AD", // Wisteria
+  "#2980B9", // Seaside Blue
+  "#16A085", // Green Sea
+  "#27AE60", // Emerald
+  "#F39C12", // Orange
+  "#D35400", // Pumpkin
+  "#C0392B", // Pomegranate
+  "#7F8C8D", // Asbestos
+  "#BDC3C7", // Silver
+  "#34495E", // Wet Asphalt
+  "#9B59B6", // Amethyst
+  "#3498DB", // Peter River
+  "#1ABC9C", // Turquoise
+  "#2ECC71", // Nephritis
+  "#F1C40F", // Sunflower
+  "#E67E22", // Carrot
+  "#E74C3C", // Alizarin
+  "#95A5A6", // Concrete
+  "#ECF0F1", // Clouds
 ];
+
 function KeepLocation({ zoomRef, centerRef }) {
   const eve = useMapEvents({
     moveend: () => {
@@ -53,15 +70,15 @@ const Mapkpitz = ({
   const zoomRef = useRef(7);
   const centerRef = useRef([31.5, 34.75]);
   const [paths, setPaths] = useState({});
-  
+
   const connection_color = (param) => {
     switch (true) {
-      case (param >= 0 && param < 10):
-        return 'green';
-      case (param >= 10 && param < 20):
-        return 'red';
+      case param >= 0 && param < 10:
+        return "green";
+      case param >= 10 && param < 20:
+        return "red";
       default:
-        return 'blue';
+        return "blue";
     }
   };
 
@@ -89,7 +106,7 @@ const Mapkpitz = ({
   // console.log("kaki", antenas["2"]);
   // console.log(typeof antenas);
   // console.log(mapData);
-  
+
   const markerIcon = new L.Icon({
     iconUrl: "/location_pin.png", // assuming Plane is the path to your icon image
     iconSize: [60, 60], // adjust the size as needed
@@ -113,15 +130,6 @@ const Mapkpitz = ({
           }
         }
       `,
-      // query: gql`
-      //     query{
-      //         row_by_time(fid: ${fid}, time: "${format(time, 'yyyy-MM-dd HH:mm:ss')}") {
-      //             params(names: [${mod_params.map(param => {return '"' + param + '"'})}]) {
-      //                 name
-      //                 value
-      //             }
-      //         }
-      //     }`
     });
     if (error) {
       console.log("Error fetching data:", error);
@@ -137,21 +145,24 @@ const Mapkpitz = ({
   }
 
   useEffect(() => {
-    var newPaths = {};
-    setPaths({}); //need to optimize
+    const previousKeys = new Set(Object.keys(paths));
+    const savedPaths = paths;
+    setPaths({});
     for (const key in mapData) {
-      var x = find_path(key);
-      console.log("OMER", x);
-      // console.log("OMER", x.marker_map);
+      if (!previousKeys.has(key)) {
+        find_path(key);
+      } else {
+        setPaths((prevPaths) => ({
+          ...prevPaths,
+          [`${key}`]: savedPaths[key],
+        }));
+      }
     }
   }, [mapData]);
 
   return (
     <>
-
       <MapContainer
-      
-
         center={centerRef.current}
         zoom={zoomRef.current}
         key={1}
@@ -163,8 +174,12 @@ const Mapkpitz = ({
         <KeepLocation zoomRef={zoomRef} centerRef={centerRef} />
 
         {showPaths &&
-          Object.keys(paths).map((key) => (
-            <Polyline positions={paths[key]} color="#00008B" />
+          Object.keys(paths).map((key, index) => (
+            <Polyline
+              key={key}
+              positions={paths[key]}
+              color={colors[index % colors.length]}
+            />
           ))}
         {/* <RotatedMarker
           key={Math.random()}
@@ -184,36 +199,59 @@ const Mapkpitz = ({
 
         {/* showing planes: */}
         {Object.keys(mapData).map((key) => {
-          
-          console.log('tele_heading:', mapData[key].tele_heading);
-          try{
-            console.log('antena:', antenas[undefined_to_number(Math.trunc(mapData[key].tele_heading / 100)) ]);
-          } catch (e) { console.log('error! ', mapData[key].tele_heading); }
+          console.log("tele_heading:", mapData[key].tele_heading);
+          try {
+            console.log(
+              "antena:",
+              antenas[
+                undefined_to_number(Math.trunc(mapData[key].tele_heading / 100))
+              ]
+            );
+          } catch (e) {
+            console.log("error! ", mapData[key].tele_heading);
+          }
 
-          return(
-          <RotatedMarker
-            key={key}
-            position={[
-              mapData[key].tele_pp_lat * (180 / Math.PI),
-              mapData[key].tele_pp_long * (180 / Math.PI),
-            ]}
-            icon={uavIcon} // Use the custom icon
-            rotationAngle={mapData[key].tele_heading}
-            rotationOrigin="center"
-          >
-            <Popup>
-              fid: {key} | altitude: {mapData[key].tele_altitude}ft, antena:{Math.floor(mapData[key].tele_heading / 100)}
-            </Popup>
-            <Polyline pathOptions={{color: connection_color(mapData[key].tele_altitude % 50)}} 
-            positions={[
-              [mapData[key].tele_pp_lat * (180 / Math.PI), 
-              mapData[key].tele_pp_long * (180 / Math.PI),], 
-              [antenas[Math.floor(undefined_to_number(mapData[key].tele_heading / 100))].lat, 
-              antenas[Math.floor(undefined_to_number(mapData[key].tele_heading / 100))].long]]} />
-          </RotatedMarker>
+          return (
+            <RotatedMarker
+              key={key}
+              position={[
+                mapData[key].tele_pp_lat * (180 / Math.PI),
+                mapData[key].tele_pp_long * (180 / Math.PI),
+              ]}
+              icon={uavIcon} // Use the custom icon
+              rotationAngle={mapData[key].tele_heading}
+              rotationOrigin="center"
+            >
+              <Popup>
+                fid: {key} | altitude: {mapData[key].tele_altitude}ft, antena:
+                {Math.floor(mapData[key].tele_heading / 100)}
+              </Popup>
+              <Polyline
+                pathOptions={{
+                  color: connection_color(mapData[key].tele_altitude % 50),
+                }}
+                positions={[
+                  [
+                    mapData[key].tele_pp_lat * (180 / Math.PI),
+                    mapData[key].tele_pp_long * (180 / Math.PI),
+                  ],
+                  [
+                    antenas[
+                      Math.floor(
+                        undefined_to_number(mapData[key].tele_heading / 100)
+                      )
+                    ].lat,
+                    antenas[
+                      Math.floor(
+                        undefined_to_number(mapData[key].tele_heading / 100)
+                      )
+                    ].long,
+                  ],
+                ]}
+              />
+            </RotatedMarker>
           );
-        }
-        )}
+        })}
 
         {/* showing heatmap: */}
         {showHeatMap && (
@@ -250,49 +288,40 @@ const Mapkpitz = ({
           })}
 
         {/* showing markers: */}
-        {showMarkerMap && (
-            markerMapData.map((type, idx) => {
-              const markerIcon = new L.Icon({
-                iconUrl: `/location_pins/location-pin(${(idx*4) % 17}).png`, // assuming Plane is the path to your icon image
-                iconSize: [60, 60], // adjust the size as needed
-                iconAnchor: [30, 30], // center the icon on the marker's position
-              });
+        {showMarkerMap &&
+          markerMapData.map((type, idx) => {
+            const markerIcon = new L.Icon({
+              iconUrl: `/location_pins/location-pin(${(idx * 4) % 17}).png`, // assuming Plane is the path to your icon image
+              iconSize: [60, 60], // adjust the size as needed
+              iconAnchor: [30, 30], // center the icon on the marker's position
+            });
+            return type.map((point) => {
               return (
-                type.map((point) => {
-                  return (
-                    <Marker
-                      key={Math.random() + Math.random()}
-                      position={[
-                        point.lat * (180 / Math.PI),
-                        point.lon * (180 / Math.PI),
-                      ]}
-                      icon={markerIcon} // Use the custom icon
-                    >
-                      <Popup>
-                        {point.content}
-                      </Popup>
-                    </Marker>
-                  );
-                })
-            );
-          })
-        )}
-        
+                <Marker
+                  key={Math.random() + Math.random()}
+                  position={[
+                    point.lat * (180 / Math.PI),
+                    point.lon * (180 / Math.PI),
+                  ]}
+                  icon={markerIcon} // Use the custom icon
+                >
+                  <Popup>{point.content}</Popup>
+                </Marker>
+              );
+            });
+          })}
 
         {/* showing antenas: */}
         {Object.keys(antenas).map((key) => (
-            <Marker
-              key={Math.random() + Math.random()}
-              position={[
-                antenas[key].lat,
-                antenas[key].long
-              ]}
-              icon={antenaIcon} // Use the custom icon
-            >
-              <Popup>
-                Antena: {key}, LAT: {antenas[key].lat}, LONG: {antenas[key].long}
-              </Popup>
-            </Marker>
+          <Marker
+            key={Math.random() + Math.random()}
+            position={[antenas[key].lat, antenas[key].long]}
+            icon={antenaIcon} // Use the custom icon
+          >
+            <Popup>
+              Antena: {key}, LAT: {antenas[key].lat}, LONG: {antenas[key].long}
+            </Popup>
+          </Marker>
         ))}
       </MapContainer>
     </>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,40 +1,75 @@
-import * as React from 'react';
-import { Typography } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import DrawerButton from '../components/DrawerButton'
-import DBWindowButton from '../components/DBWindowButton'
+import * as React from "react";
+import { Typography } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import DrawerButton from "../components/DrawerButton";
+import DBWindowButton from "../components/DBWindowButton";
 
-export default function MenuAppBar({insertedQueryJson, switch1, updateSwitch1, barSpeed, 
-  updateBarSpeed, setDBWindowOpen, DBWindowOpen, setHeatMapData, setShowHeatMap, setFlights,
-  setMarkerMapData, query_num, setQuery_num, setQueriesDict, QueriesDict, setShowMarkerMap, allParams, params, setParams,
-  }) {
+export default function MenuAppBar({
+  insertedQueryJson,
+  switch1,
+  updateSwitch1,
+  barSpeed,
+  updateBarSpeed,
+  setDBWindowOpen,
+  DBWindowOpen,
+  setHeatMapData,
+  setShowHeatMap,
+  setFlights,
+  setMarkerMapData,
+  query_num,
+  setQuery_num,
+  setQueriesDict,
+  QueriesDict,
+  setShowMarkerMap,
+  allParams,
+  params,
+  setParams,
+}) {
   return (
-    <Box sx={{ width: "100vw"}}>
+    <Box sx={{ width: "100vw" }}>
       <AppBar position="static" color="primary">
-        <Toolbar>
-        {/* <Typography
-            variant="h4"
-            fontFamily={'"Segoe UI"'}
-            fontWeight={"bold"}
-            color={"white"}
+        <Toolbar sx={{ position: "relative" }}>
+          <DrawerButton
+            insertedQueryJson={insertedQueryJson}
+            switch1={switch1}
+            setSwitch1={updateSwitch1}
+            barSpeed={barSpeed}
+            updateBarSpeed={updateBarSpeed}
+            setHeatMapData={setHeatMapData}
+            setShowHeatMap={setShowHeatMap}
+            setFlights={setFlights}
+            setMarkerMapData={setMarkerMapData}
+            query_num={query_num}
+            setQuery_num={setQuery_num}
+            setQueriesDict={setQueriesDict}
+            QueriesDict={QueriesDict}
+            setShowMarkerMap={setShowMarkerMap}
+            allParams={allParams}
+            params={params}
+            setParams={setParams}
+          />
+          <Box
             sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              position: "absolute"
+              position: "absolute",
+              left: "50%",
+              transform: "translateX(-50%)",
             }}
           >
-            LionFish
-          </Typography> */}
-          <DrawerButton insertedQueryJson={insertedQueryJson} 
-          switch1={switch1} setSwitch1={updateSwitch1} 
-          barSpeed={barSpeed} updateBarSpeed={updateBarSpeed}
-          setHeatMapData={setHeatMapData} setShowHeatMap={setShowHeatMap} setFlights={setFlights} setMarkerMapData={setMarkerMapData} query_num={query_num} setQuery_num={setQuery_num} setQueriesDict={setQueriesDict} QueriesDict={QueriesDict} setShowMarkerMap={setShowMarkerMap} allParams={allParams} params={params} setParams={setParams}/>
-
-          <DBWindowButton setWindowOpen={setDBWindowOpen} windowOpen={DBWindowOpen}/>
-
+            <Typography
+              variant="h4"
+              fontFamily={'"Segoe UI"'}
+              fontWeight={"bold"}
+              color={"white"}
+            >
+              LionFish
+            </Typography>
+          </Box>
+          <DBWindowButton
+            setWindowOpen={setDBWindowOpen}
+            windowOpen={DBWindowOpen}
+          />
         </Toolbar>
       </AppBar>
     </Box>
